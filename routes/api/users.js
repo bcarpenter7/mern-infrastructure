2ce78bbf3aa5484be592f755a8146a93dd4d0f2d
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-07-26 14:26:44 -0400

    final

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const router = express.Router();
 const usersCtrl = require('../../controllers/api/users')
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
 
 
 // All paths start with '/api/users'
 
 // POST /api/users
 router.post('/', usersCtrl.create);
+
+router.post('/login', usersCtrl.login)
+///api/users/checktoken
+router.get('/check-token', usersCtrl.checkToken)
 
 module.exports = router;
